ed9ef269abe7422eb1247b59fff2dadd4a509d33
Ashantih2 <ashantihill2@gmail.com>
2026-06-25 11:53:53 -0400

    Committed changes to M7

---
+++ PalmerPenguinsM7.java
@@ -0,0 +1,31 @@
+// PalmerPenguinsM7.java
+// Ashanti Hill
+// June 28, 2026
+// Refactored Palmer Penguins analysis using object-oriented approach
+
+public class PalmerPenguinsM7 {
+    public static void main(String[] args) {
+        // Create an instance of SpeciesCounter
+        SpeciesCounter counter = new SpeciesCounter();
+
+        // Read species data
+        counter.readSpeciesData();
+        
+
+        // Initialize species count
+        counter.initializeSpeciesCount();
+
+        // Check if data is empty
+        if (counter.isDataEmpty()) {
+            System.out.println(
+                "Error: The file is empty or could not be read.");
+            return;
+        }
+
+        // Count species occurrences
+        counter.countSpecies();
+
+        // Print results
+        counter.printSpeciesCount();
+    }
+}
